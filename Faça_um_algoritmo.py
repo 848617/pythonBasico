46c65e68f278fcc0ef937e632c555d0937eb4ac0
848617 <124939494+848617@users.noreply.github.com>
2023-02-09 20:37:49 -0300

    Add files via upload

---
+++ Faça_um_algoritmo.py
@@ -0,0 +1,12 @@
+nota1 = float(input("informe a 1 Nota"))
+nota2 = float(input("informe a 2 Nota"))
+nota3 = float(input("informe a 3 Nota"))
+
+Média = (nota1 + nota2 + nota3)/3
+print(Média)
+
+if Média >=7.0:
+    print("aluno aprovado")
+else:
+    print ("aluno reprovado")
+    
