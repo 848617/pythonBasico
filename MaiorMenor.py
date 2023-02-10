0bf7724947666885d220d3cac68d7f320fada918
848617 <124939494+848617@users.noreply.github.com>
2023-02-09 21:40:45 -0300

    Add files via upload

---
+++ MaiorMenor.py
@@ -0,0 +1,24 @@
+a = int(input('informe valor do a'))
+maior=a
+menor=a
+
+b= int(input('informe valor do b'))
+
+if b>maior
+	maior = b
+
+else:
+	menor = b
+
+c= int(input('informe valor do c'))
+
+if c>maior
+	maior=c
+
+if< menor
+	
+	print(a>b>c)
+
+
+
+
