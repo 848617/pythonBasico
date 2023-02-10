50a263f7fa313f091f49808b34e209514ca3e0b1
848617 <124939494+848617@users.noreply.github.com>
2023-02-09 21:41:04 -0300

    Add files via upload

---
+++ Velocidade.py
@@ -0,0 +1,9 @@
+""" Velocidade = float(input("Digite sua Velocidade"))
+
+valormulta = (Velocidade-80)*5
+
+if Velocidade > 80:
+	print("Voce foi multado!")
+	print (f" valor da multa é {valormulta}")
+ """
+ 
